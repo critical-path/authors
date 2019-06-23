357c021cf2ceb0667e7b99646f23d48deeb25db0
critical-path <37241479+critical-path@users.noreply.github.com>
2019-06-23 11:20:12 -0400

    Improve tests.

---
+++ test/conftest.py
@@ -0,0 +1,109 @@
+import io
+import pytest
+import authors
+import constants
+
+
+# This represents the result of redirecting `git log --format=%an`
+# to standard input.  Standard input is blocked by pytest,
+# so we need a mock-up.
+
+@pytest.fixture
+def standard_input(monkeypatch):
+    git_log = "Test Author C\nTest Author B\nTest Author A\n"
+    return monkeypatch.setattr("sys.stdin", io.StringIO(git_log))
+
+
+# This represents a configuration file for which the value of
+# `kind` is `md`.
+
+@pytest.fixture
+def configuration_file(tmp_path):
+    file = tmp_path.joinpath(constants.CONFIGURATION_FILE)
+    file.write_text(constants.YAML_WITH_MD_KIND)
+    yield file
+
+
+# This represents a configuration file for which the value of
+# `kind` is `html`.
+
+@pytest.fixture
+def configuration_file_with_html_kind(tmp_path):
+    file = tmp_path.joinpath(constants.CONFIGURATION_FILE)
+    file.write_text(constants.YAML_WITH_HTML_KIND)
+    yield file
+
+
+# This represents a configuration file for which the value of
+# `kind` is `rst`.
+
+@pytest.fixture
+def configuration_file_with_rst_kind(tmp_path):
+    file = tmp_path.joinpath(constants.CONFIGURATION_FILE)
+    file.write_text(constants.YAML_WITH_RST_KIND)
+    yield file
+
+
+# This represents a configuration file for which the value of
+# `kind` is `invalid`.
+
+@pytest.fixture
+def configuration_file_with_invalid_kind(tmp_path):
+    file = tmp_path.joinpath(constants.CONFIGURATION_FILE)
+    file.write_text(constants.YAML_WITH_INVALID_KIND)
+    yield file
+
+
+# This represents valid configuration settings parsed from a
+# file.  The value of `kind` is `md`, so it will produce
+# an AUTHORS file in MD format.
+
+@pytest.fixture
+def configuration(configuration_file):
+    yaml = authors.lib.read_configuration_file(file=configuration_file)
+    return authors.lib.parse_yaml_to_dict(yaml)
+
+
+# This represents valid configuration settings parsed from a
+# file.  The value of `kind` is `html`, so it will produce
+# an AUTHORS file in HTML format.
+
+@pytest.fixture
+def configuration_with_html_kind(configuration_file_with_html_kind):
+    yaml = authors.lib.read_configuration_file(file=configuration_file_with_html_kind)
+    return authors.lib.parse_yaml_to_dict(yaml)
+
+
+# This represents valid configuration settings parsed from a
+# file.  The value of `kind` is `rst`, so it will produce
+# an AUTHORS file in RST format.
+
+@pytest.fixture
+def configuration_with_rst_kind(configuration_file_with_rst_kind):
+    yaml = authors.lib.read_configuration_file(file=configuration_file_with_rst_kind)
+    return authors.lib.parse_yaml_to_dict(yaml)
+
+
+# This represents invalid configuration settings parsed from a
+# file.  The value of `kind` is `invalid`, so it will produce
+# an AUTHORS file in the default format (MD).
+
+@pytest.fixture
+def configuration_with_invalid_kind(configuration_file_with_invalid_kind):
+    yaml = authors.lib.read_configuration_file(file=configuration_file_with_invalid_kind)
+    return authors.lib.parse_yaml_to_dict(yaml)
+
+
+# This represents a list of authors, the result of calling
+# `authors.lib.read_standard_input`.
+
+@pytest.fixture
+def authors_list():
+    return ["Test Author A", "Test Author B", "Test Author C"]
+
+
+# This represents a rendered template.
+
+@pytest.fixture
+def rendered_template(authors_list, configuration):
+    return authors.lib.render_template(authors_list, configuration)
---
+++ test/test_lib.py
@@ -0,0 +1,84 @@
+import pathlib
+import authors
+import constants
+
+
+def test_read_configuration_file(configuration_file):
+    results = authors.lib.read_configuration_file(file=configuration_file)
+    assert results == constants.YAML_WITH_MD_KIND
+
+
+def test_read_configuration_file_when_file_does_not_exist():
+    file = ".this-file-does-not-exist.yml"
+    results = authors.lib.read_configuration_file(file=file)
+    assert results == str()
+
+
+def test_parse_yaml_to_dict():
+    results = authors.lib.parse_yaml_to_dict(constants.YAML_WITH_MD_KIND)
+    assert isinstance(results, dict)
+    assert results["name"] == "TEST-AUTHORS"
+    assert results["kind"] == "md"
+    assert results["heading"] == "Test Heading"
+    assert results["opening"] == "Test Opening"
+    assert results["closing"] == "Test Closing"
+
+
+def test_parse_yaml_to_dict_when_yaml_is_empty_str():
+    yaml = str()
+    results = authors.lib.parse_yaml_to_dict(yaml)
+    assert results == dict()
+
+
+def test_validate_configuration(configuration):
+    results = authors.lib.validate_configuration(configuration)
+    assert results == configuration
+
+
+def test_validate_configuration_when_configuration_is_invalid(configuration_with_invalid_kind):
+    results = authors.lib.validate_configuration(configuration_with_invalid_kind)
+    assert results["name"] == "TEST-AUTHORS"
+    assert results["kind"] == "md"
+    assert results["heading"] == "Test Heading"
+    assert results["opening"] == "Test Opening"
+    assert results["closing"] == "Test Closing"
+
+
+def test_validate_configuration_when_configuration_is_empty_dict():
+    configuration = dict()
+    results = authors.lib.validate_configuration(configuration)
+    assert results == authors.lib.DEFAULT_CONFIGURATION
+
+
+def test_read_standard_input(standard_input):
+    results = authors.lib.read_standard_input()
+    assert results == ["Test Author A", "Test Author B", "Test Author C"]
+
+
+def test_render_template(authors_list, configuration):
+    results = authors.lib.render_template(authors_list, configuration)
+    assert results == constants.AUTHORS_WITH_MD_KIND
+
+
+def test_render_template_with_html_kind(authors_list, configuration_with_html_kind):
+    results = authors.lib.render_template(authors_list, configuration_with_html_kind)
+    assert results == constants.AUTHORS_WITH_HTML_KIND
+
+
+def test_render_template_with_rst_kind(authors_list, configuration_with_rst_kind):
+    results = authors.lib.render_template(authors_list, configuration_with_rst_kind)
+    assert results == constants.AUTHORS_WITH_RST_KIND
+
+
+def test_write_authors_file(rendered_template, configuration):
+    authors.lib.write_authors_file(rendered_template, configuration)
+    destination_file = pathlib.Path(configuration["name"])
+    assert destination_file.exists()
+    destination_file.unlink()
+
+
+def test_main(standard_input, configuration_file, configuration):
+    authors.lib.main(file=configuration_file)
+    destination_file = pathlib.Path(configuration["name"])
+    assert destination_file.exists()
+    destination_file.unlink()
