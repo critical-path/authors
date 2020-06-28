371ddd3ed7f5ad04dd1a9da11451c38138abb37c
critical-path <37241479+critical-path@users.noreply.github.com>
2020-06-28 15:28:55 -0400

    Use `pathlib` consistently.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="authors",
-    version="0.3.1",
+    version="0.3.2",
     description="Thank the people who contribute to your Git/GitHub project by creating an AUTHORS file.",
     url="https://github.com/critical-path/authors",
     author="critical-path",
